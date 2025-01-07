a3275a00bd97f08ad7dedf2e8896db7846e745c2
chily2265 <hazakachi@gmail.com>
2025-01-08 01:15:39 +0700

    update frontservice value

--- Chat-Service/index.js
+++ Chat-Service/index.js
@@ -18,7 +18,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: `${frontendUrl}`,
+    origin: frontendUrl,
     methods: ["GET", "POST"],
     credentials: true,
   },
