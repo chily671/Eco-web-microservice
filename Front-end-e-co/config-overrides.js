a81c6fd8e3499c6b943b6f2f6c3e003a35dd8623
chily2265 <hazakachi@gmail.com>
2025-01-08 02:26:47 +0700

    test fix bug

---
+++ Front-end-e-co/config-overrides.js
@@ -0,0 +1,9 @@
+module.exports = function override(config) {
+    config.resolve.fallback = {
+      fs: false,
+      path: require.resolve('path-browserify'),
+      os: require.resolve('os-browserify/browser'),
+    };
+    return config;
+  };
+  
